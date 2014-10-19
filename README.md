c117a3936091733d947d361eb75e6fb39c64451e
landlessness <brian@landlessness.net>
2014-10-19 03:29:02 -0400

    fixed turnon

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a LED device for use in the Zetta platform on Beaglebone
 
-##Installation
+##Install
 
 `npm install zetta-led-bonescript-driver`
 
@@ -20,3 +20,29 @@ zetta()
     console.log('Zetta is running at http://beaglebone.local:1337');
   });
 ```
+
+### Hardware
+
+* [Beagle Bone](http://beagleboard.org/black)
+
+###Transitions
+
+#####turn-on
+
+No arguments. Turns on the led continuously.
+
+#####turn-on-pulse
+
+No arguments. Alternately turn the led on for 150 ms then off for 100ms.
+
+#####turn-on-alternating
+
+No arguments. Alternately turns on the led for 100 ms then off for 400ms.
+
+#####turn-off
+
+No arguments. Turns off the beeping.
+
+#####flash
+
+No arguments. Flashes the led once.
--- led.js
+++ led.js
@@ -2,94 +2,99 @@ var Device = require('zetta-device');
 var util = require('util');
 var bone = require('bonescript');
 
-var LED = module.exports = function(pin) {
+var Buzzer = module.exports = function(pin) {
   Device.call(this);
-  this.pin = pin;
+  this.pin = pin || 'P9_14';
 };
-util.inherits(LED, Device);
+util.inherits(Buzzer, Device);
 
-LED.prototype.init = function(config) {
+Buzzer.prototype.init = function(config) {
   config
-    .type('led')
-    .name('led ' + this.pin)
     .state('off')
-    .when('off', { allow: ['turn-on', 'turn-on-pulse', 'turn-on-alternating', 'flash']})
-    .when('on', { allow: ['turn-off', 'turn-on-pulse', 'turn-on-alternating', 'flash'] })
-    .when('pulse', { allow: ['turn-off', 'turn-on', 'turn-on-alternating', 'flash'] })
-    .when('alternating', { allow: ['turn-off', 'turn-on', 'turn-on-pulse', 'flash'] })
-    .when('flash', { allow: [] })
-    .map('flash', this.flash)
+    .type('buzzer')
+    .name('Buzzer')
+    .when('off', { allow: ['turn-on', 'turn-on-pulse', 'turn-on-alternating', 'beep']})
+    .when('on', { allow: ['turn-off', 'turn-on-pulse', 'turn-on-alternating', 'beep'] })
+    .when('pulse', { allow: ['turn-off', 'turn-on', 'turn-on-alternating', 'beep'] })
+    .when('alternating', { allow: ['turn-off', 'turn-on', 'turn-on-pulse', 'beep'] })
+    .when('beep', { allow: [] })
+    .map('beep', this.beep)
     .map('turn-on', this.turnOn)
     .map('turn-on-pulse', this.turnOnPulse)
     .map('turn-on-alternating', this.turnOnAlternating)
     .map('turn-off', this.turnOff);
-    
+
   //Everything is off to start
-  bone.pinMode(this.pin, bone.OUTPUT);
-  bone.digitalWrite(this.pin, 0);
+  bone.analogWrite(this.pin, 0);
 };
 
-LED.prototype.turnOn = function(cb) {
+// Swedish Standard SS 03 17 11, No. 4 “All clear”
+// Continuous 500 Hz
+Buzzer.prototype.turnOn = function(cb) {
   var state = 'on';
-  var onDuration = 500;
+  var freq = 500;
+  var onDuration = Infinity;
   var offDuration = 0;
-  this._pattern(onDuration, offDuration, state, cb);
+  this._pattern(freq, onDuration, offDuration, state, cb);
 };
 
-LED.prototype.turnOnPulse = function(cb) {
+// Swedish Standard SS 03 17 11, No. 1 “Imminent Danger”
+// Pulse tone 500 Hz
+Buzzer.prototype.turnOnPulse = function(cb) {
   var state = 'pulse';
+  var freq = 500;
   var onDuration = 150;
   var offDuration = 100;
-  this._pattern(onDuration, offDuration, state, cb);
+  this._pattern(freq, onDuration, offDuration, state, cb);
 };
 
-LED.prototype.turnOnAlternating = function(cb) {
+// “French fire sound” NF S 32-001-1975
+// Pulse tone 560 Hz
+Buzzer.prototype.turnOnAlternating = function(cb) {
   var state = 'alternating';
+  var freq = 560;
   var onDuration = 100;
   var offDuration = 400;
-  this._pattern(onDuration, offDuration, state, cb);
+  this._pattern(freq, onDuration, offDuration, state, cb);
 };
 
-LED.prototype.flash = function(cb) {
-  this.state = 'flash';
+Buzzer.prototype.beep = function(cb) {
+  this.state = 'beep';
   var self = this;
   this.turnOff(function() {
-    self._emit(100, 750); 
+    self._buzz(100, 750); 
   });
   cb();
 };
 
-
-LED.prototype.turnOff = function(cb) {  
+Buzzer.prototype.turnOff = function(cb) {
   if (this._timer != undefined) {
     clearInterval(this._timer);
   }
-  var self = this;
-  bone.digitalWrite(this.pin, 0, function() {
-    self.state = 'off';
-    cb();
-  });
+  bone.analogWrite(this.pin, 0);
+  this.state = 'off';
+  cb();
 };
 
-
-LED.prototype._pattern = function(onDuration, offDuration, state, cb) {
+Buzzer.prototype._pattern = function(freq, onDuration, offDuration, state, cb) {
   var self = this;
   this.turnOff(function(){
-    self._timer = setInterval(self._emit.bind(self, onDuration), onDuration + offDuration);
+    if (onDuration === Infinity || offDuration === 0) {
+      bone.analogWrite(self.pin, 0.5, freq);
+    } else {
+      self._timer = setInterval(self._buzz.bind(self, onDuration, freq), onDuration + offDuration);
+    }
     self.state = state;
     cb();
   });
 };
 
-LED.prototype._emit = function(delay) {
+Buzzer.prototype._buzz = function(delay, freq) {
   var self = this;
-  
-  bone.digitalWrite(this.pin, 1, function() {
+  bone.analogWrite(this.pin, 0.5, freq, function() {
     setTimeout(function() {
-      bone.digitalWrite(self.pin, 0);
+      bone.analogWrite(self.pin, 0, freq);
     }, delay);
   });
-
 };
-
 
